e87fb8a5936c0b4b0bd9bb66ecb196d276852759
Julien WALLE <jwalle@e2r11p22.42.fr>
2015-06-19 22:10:48 +0200

    fim

--- D03/ex06/members_only.php
+++ D03/ex06/members_only.php
@@ -5,21 +5,21 @@
 	if ($_SERVER['PHP_AUTH_PW'])
 		$pw = $_SERVER['PHP_AUTH_PW'];
 
-
-	if ($user.":".$pw === "zaz:jaimelespetitsponeys")
+	//if ($user.":".$pw === "zaz:jaimelespetitsponeys")
+	if ($user.":".$pw === "zaz:jaime")
 	{
 		
-		$base_img = base64_encode(file_get_contents("../img/42.png"));
-		echo "<html><body>\nBonjour Zaz<br />\n";
-		echo "<img src='data:image/png;base64"
-		echo $base_img;
-		echo ">\n";
+		//$base_img = base64_encode(file_get_contents("../img/42.png"));
+		echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64,";
+		echo base64_encode(file_get_contents("../img/42.png"));
+		//echo $base_img;
+		echo "'>\n";
 		echo "</body></html>\n";
 	}
 	else
 	{
-		header('Connection: close');
-		//header();
-		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n* Closing connection #0\n";
+		header('WWW-Authenticate: Basic realm="Espace Membres"');
+		header('HTTP/1.0 401 Unauthorized');
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
 	}
 ?>
