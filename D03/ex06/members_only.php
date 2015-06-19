f0023a44619f94411434d90368a36f00bd6ae488
Julien WALLE <jwalle@e2r11p22.42.fr>
2015-06-19 21:00:46 +0200

    vite

--- D03/ex06/members_only.php
+++ D03/ex06/members_only.php
@@ -1,5 +1,25 @@
 <?PHP
 
+	if ($_SERVER['PHP_AUTH_USER'])
+		$user = $_SERVER['PHP_AUTH_USER'];
+	if ($_SERVER['PHP_AUTH_PW'])
+		$pw = $_SERVER['PHP_AUTH_PW'];
 
 
+	if ($user.":".$pw === "zaz:jaimelespetitsponeys")
+	{
+		
+		$base_img = base64_encode(file_get_contents("../img/42.png"));
+		echo "<html><body>\nBonjour Zaz<br />\n";
+		echo "<img src='data:image/png;base64"
+		echo $base_img;
+		echo ">\n";
+		echo "</body></html>\n";
+	}
+	else
+	{
+		header('Connection: close');
+		//header();
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n* Closing connection #0\n";
+	}
 ?>
