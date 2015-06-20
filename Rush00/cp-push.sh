3d2bfa65a17b23157c547d1eda98b00a49137614
Julien WALLE <jwalle@e2r12p21.42.fr>
2015-06-20 23:00:59 +0200

    quick

--- Rush00/cp-push.sh
+++ Rush00/cp-push.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+echo \\\\\\ cp -R  ~/mamp/apps/chupa/htdocs/ . //////
 cp -R  ~/mamp/apps/chupa/htdocs/ .
+
+echo \\\\\\\git add -A/////////
 git add -A
+
+echo \\\\\git commit -m "quick"//////
 git commit -m "quick"
+
+echo \\\\\\git push origin master/////
 git push origin master
